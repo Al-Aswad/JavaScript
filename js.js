c1638e72899fe087dcbf18246937cdd9d584102c
Hajar Aswad <hajaraswadkom@gmail.com>
2021-09-30 16:54:14 +0800

    perulangan for dan for each

---
+++ js.js
@@ -0,0 +1,267 @@
+let replay = [{
+        "id": "ACR290047-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "-7",
+        "date": "Jadwal Progress",
+        "arrival": null,
+        "description": "Keterangan",
+        "status": null,
+        "create_at": "2021-09-29 02:00:47",
+        "last_update": "2021-09-29 02:00:47"
+    },
+    {
+        "id": "ACR293914-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": "11 September 2021",
+        "arrival": null,
+        "description": "Konsumen akan datang ke cabang pettarani",
+        "status": null,
+        "create_at": "2021-09-29 13:39:14",
+        "last_update": "2021-09-29 13:39:14"
+    },
+    {
+        "id": "ACR294249-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290307-001",
+        "date": "24 September 2021",
+        "arrival": null,
+        "description": "KKeterangan terjadwal",
+        "status": null,
+        "create_at": "2021-09-29 13:42:49",
+        "last_update": "2021-09-29 13:42:49"
+    },
+    {
+        "id": "ACR294530-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": "25 September 2021",
+        "arrival": null,
+        "description": "Cabang pettarani terjadwal",
+        "status": null,
+        "create_at": "2021-09-29 13:45:30",
+        "last_update": "2021-09-29 13:45:30"
+    },
+    {
+        "id": "ACR294845-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": "23 September 2021",
+        "arrival": null,
+        "description": "asaa",
+        "status": null,
+        "create_at": "2021-09-29 13:48:45",
+        "last_update": "2021-09-29 13:48:45"
+    },
+    {
+        "id": "ACR294929-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": null,
+        "arrival": null,
+        "description": "Menolak Whatsapp",
+        "status": null,
+        "create_at": "2021-09-29 02:49:29",
+        "last_update": "2021-09-29 02:49:29"
+    },
+    {
+        "id": "ACR295204-001",
+        "user_id": null,
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC295131-001",
+        "date": "sebulan kedepan",
+        "arrival": null,
+        "description": "Keterangan",
+        "status": null,
+        "create_at": "2021-09-29 02:52:04",
+        "last_update": "2021-09-29 02:52:04"
+    },
+    {
+        "id": "ACR295345-001",
+        "user_id": "k21091309001",
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": "9 September 2021",
+        "arrival": "PT. SINAR GALESONG MANDIR",
+        "description": "sadasfas",
+        "status": null,
+        "create_at": "2021-09-29 13:53:45",
+        "last_update": "2021-09-29 13:53:45"
+    },
+    {
+        "id": "ACR300445-001",
+        "user_id": "k21091309001",
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC294246-001",
+        "date": "15 September 2021",
+        "arrival": "PT. SINAR GALESONG MANDIR",
+        "description": "Keterangan konsumen akan datang tanggal 15 september",
+        "status": null,
+        "create_at": "2021-09-30 00:04:45",
+        "last_update": "2021-09-30 00:04:45"
+    },
+    {
+        "id": "ACR300605-001",
+        "user_id": "k21091309001",
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC290204-001",
+        "date": "23 September 2021",
+        "arrival": "PT. SINAR GALESONG MANDIR",
+        "description": "guguyguyg",
+        "status": "-7",
+        "create_at": "2021-09-30 00:06:05",
+        "last_update": "2021-09-30 00:06:05"
+    },
+    {
+        "id": "ACR305507-001",
+        "user_id": "k21091309001",
+        "vin": "146242450003UX110NER ACCS",
+        "activity_id": "AC303624-001",
+        "date": null,
+        "arrival": null,
+        "description": "pending",
+        "status": "-8",
+        "create_at": "2021-09-30 05:55:07",
+        "last_update": "2021-09-30 05:55:07"
+    }
+];
+
+let rstatus = [{
+        "id": "ACS223617-001",
+        "kode": "-9",
+        "user_id": "",
+        "name": "whatsapp - menolak",
+        "description": "",
+        "create_at": "2021-09-22 08:36:17",
+        "last_update": "2021-09-22 08:37:57"
+    },
+    {
+        "id": "ACS223616-001",
+        "kode": "-7",
+        "user_id": "",
+        "name": "whatsapp - terjadwal",
+        "description": "",
+        "create_at": "2021-09-22 08:36:16",
+        "last_update": "2021-09-22 08:37:49"
+    },
+    {
+        "id": "ACS223616-002",
+        "kode": "-8",
+        "user_id": "",
+        "name": "whatsapp - pending",
+        "description": "",
+        "create_at": "2021-09-22 08:36:16",
+        "last_update": "2021-09-22 08:37:54"
+    },
+    {
+        "id": "ACS223614-001",
+        "kode": "-6",
+        "user_id": "",
+        "name": "whatsapp - read",
+        "description": "",
+        "create_at": "2021-09-22 08:36:14",
+        "last_update": "2021-09-22 08:37:46"
+    },
+    {
+        "id": "ACS223613-001",
+        "kode": "-5",
+        "user_id": "",
+        "name": "whatsapp - centang 2",
+        "description": "",
+        "create_at": "2021-09-22 08:36:13",
+        "last_update": "2021-09-22 08:37:43"
+    },
+    {
+        "id": "ACS223020-001",
+        "kode": "-4",
+        "user_id": "",
+        "name": "whatsapp - centang 1",
+        "description": "",
+        "create_at": "2021-09-22 08:30:20",
+        "last_update": "2021-09-22 08:36:07"
+    },
+    {
+        "id": "ACS222941-001",
+        "kode": "-3",
+        "user_id": "",
+        "name": "call - ditolak",
+        "description": "",
+        "create_at": "2021-09-22 08:29:41",
+        "last_update": "2021-09-22 08:35:32"
+    },
+    {
+        "id": "ACS222937-001",
+        "kode": "-2",
+        "user_id": "",
+        "name": "call - pending",
+        "description": "",
+        "create_at": "2021-09-22 08:29:37",
+        "last_update": "2021-09-22 08:35:15"
+    },
+    {
+        "id": "ACS222759-001",
+        "kode": "-1",
+        "user_id": "",
+        "name": "call - tidak aktif",
+        "description": "",
+        "create_at": "2021-09-22 08:27:59",
+        "last_update": "2021-09-22 08:35:01"
+    },
+    {
+        "id": "ACS222745-001",
+        "kode": "1",
+        "user_id": "",
+        "name": "call - terjadwal",
+        "description": "",
+        "create_at": "2021-09-22 08:27:45",
+        "last_update": "2021-09-22 08:34:32"
+    },
+    {
+        "id": "ACS222739-001",
+        "kode": "0",
+        "user_id": "",
+        "name": "call - tidak tersambung",
+        "description": "",
+        "create_at": "2021-09-22 08:27:39",
+        "last_update": "2021-09-22 08:34:09"
+    }
+];
+
+
+// replay.forEach((data) => {
+//     data['namaStatus'] = "Kosong"
+//     data.status;
+//     rstatus.forEach((s) => {
+//         if (data.status == s.kode) {
+//             data['namaStatus'] = s.name
+//         }
+//     })
+// })
+
+
+
+// console.log(replay);
+
+
+for (x in replay) {
+    console.log(replay[x].id);
+    replay[x].namaStatus = "cek";
+}
+
+console.log(replay);
+
+
+
+
+
+
+
+// replay.forEach((data) => {
+//     data['namaUnit'] = 'namaunit';
+// });
